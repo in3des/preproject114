82ded7d7a9473ab373c3706fb20c3340afe2a20a
in3des <62700347+in3des@users.noreply.github.com>
2021-06-27 17:35:24 +0400

    delete trash files

--- target/test-classes/test.sql
+++
@@ -1,6 +0,0 @@
-#insert into users (name, lastName, age) values ('Alex', 'Balabanov', 22);
-#select * from users;
-#select age from users where id = 1;
-#update users set name='Bob', lastName='Thornton', age=58 where id = 6;
-#delete from users;
-#delete from users where id = 4;
